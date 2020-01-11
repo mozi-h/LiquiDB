ab9ce2e84bc17fca00b2705c9855cc070f8bd5b3
mozi-h <die-123-321@web.de>
2020-01-12 00:13:06 +0100

    Teilnehmer-Auflistung hinzugefügt

---
+++ abzeichen/teilnehmer.php
@@ -0,0 +1,67 @@
+<?php
+  require_once("../config.php");
+  set_relpath(1);
+
+  restricted("Trainer");
+
+  
+?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+  <?= get_head() ?>
+  
+  <title>Teilnehmer | LiquiDB</title>
+</head>
+<body>
+  <?= get_nav("abzeichen") ?>
+  <div class="container">
+    <h1 class="text-info display-4 text-center mdi mdi-account-outline"> Teilnehmer</h1>
+    <?= catch_alert() ?>
+    <table class="table table-striped text-center" id="datatable">
+      <thead>
+        <th></th>
+        <th data-priority=1>Name</th>
+        <th data-priority=700>Geburtstag</th>
+        <th data-priority=600>Alter</th>
+        <th data-priority=870>Ort</th>
+        <th data-priority=10000>Notiz</th>
+        <th data-priority=10000>Eingepflegt von</th>
+      </thead>
+      <tbody>
+        <!--AJAX-->
+      </tbody>
+    </table>
+  </div>
+  
+  <?= get_foot() ?>
+  <script>
+    // DataTable aktivieren
+    $(document).ready(function() {
+      $('#datatable').DataTable( {
+        "language": {
+          url: "<?= RELPATH ?>js/DataTables/german.json"
+        },
+        "ajax": "<?= RELPATH ?>ajax/abzeichen/teilnehmer.php",
+        "responsive": { // WIP - RESPONSIVE DATATABLE
+        details: {
+              type: "column"
+            }
+          },
+        "order": [
+          [1, "asc"]
+        ],
+        "columns": [
+          {orderable: false},
+          null,
+          null,
+          null,
+          null,
+          null,
+          null
+        ]
+      });
+    });
+  </script>
+</body>
+</html>
