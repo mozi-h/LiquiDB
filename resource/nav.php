9d9256a5ff5bdcbeb0f7eb73bbe120cad1bf5061
mozi-h <die-123-321@web.de>
2020-02-13 18:02:33 +0100

    Rollen-Symbole rechts in der Navleiste

--- resource/nav.php
+++ resource/nav.php
@@ -14,6 +14,17 @@
   <?php
     if(isset($_SESSION["USER"])) {
       $user = get_user($_SESSION["USER"]);
+      // Gruppen-Icons generieren
+      $icons = "";
+      if($user["ist_trainer"]) {
+        $icons .= "<span class='mdi mdi-account-star'></span>";
+      }
+      if($user["ist_admin"]) {
+        $icons .= "<span class='mdi mdi-shield-account'></span>";
+      }
+      if(!empty($icons)) {
+        $icons .= " | ";
+      }
       ?>
       <li class="nav-item ml-1">
         <a class="nav-link <?= $tab["eintritt"] ?? "" ?>" href="<?= RELPATH ?>eintritt/index.php">Eintritt</a>
@@ -25,7 +36,7 @@
         <a class="nav-link <?= $tab["admin"] ?? "" ?> <?= ["disabled", ""][$user["ist_admin"]] ?>" href="<?= RELPATH ?>admin/index.php">Admin</a>
       </li>
       <li class="nav-item dropdown ml-auto">
-        <a class="nav-link dropdown-toggle <?= $tab["nutzer"] ?? "" ?>" data-toggle="dropdown" href="#" role="button"><?= $user["display_name_esc"] ?></a>
+        <a class="nav-link dropdown-toggle <?= $tab["nutzer"] ?? "" ?>" data-toggle="dropdown" href="#" role="button"><?= $icons ?><?= $user["display_name_esc"] ?></a>
         <div class="dropdown-menu dropdown-menu-right">
           <a class="dropdown-item mdi mdi-account-edit" href="<?= RELPATH ?>user/change-data.php">Daten ändern</a>
           <div class="dropdown-divider"></div>
