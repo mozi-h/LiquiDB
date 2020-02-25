7c77dce325f062b6433cdfd0ef5a7b4df0f4ada8
mozi_h <mozi_h@icloud.com>
2020-02-25 13:38:08 +0100

    disziplin löschen/neu verbessert

--- abzeichen/disziplin-löschen-senden.php
+++ abzeichen/disziplin-löschen-senden.php
@@ -22,6 +22,12 @@
     // id keine Disziplin
     send_alert($target, "warning", "ID ist keine Disziplin");
   }
+  $badge = get_badge(mysqli_fetch_array($result)[0]);
+  // ist das Abzeichen noch in Arbeit?
+  if($badge["status"] != "WIP") {
+    // Abzeichen gesetzt / ausgestellt
+    send_alert($target, "warning", "Abzeichen ist bereits ausgestellt");
+  }
 
   // Disziplin löschen
   $query = sprintf(
@@ -32,5 +38,5 @@
     // Fehler beim Query
     send_alert($target, "danger", "Fehler: " . mysqli_error($db));
   }
-  send_alert(RELPATH . "abzeichen/abzeichen-bearbeiten.php?id=" . mysqli_fetch_array($result)["badge_id"] . "#disziplinen", "success", "Disziplin gelöscht");
+  send_alert(RELPATH . "abzeichen/abzeichen-bearbeiten.php?id=" . $badge["id"] . "#disziplinen", "success", "Disziplin gelöscht");
 ?>
